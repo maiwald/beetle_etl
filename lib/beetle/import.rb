07a661ed29a9785491dba06a09917190d8290e39
Luciano Maiwald <luciano.maiwald@gmail.com>
2014-08-01 13:42:05 +0200

    refactor Beetle::Import

--- lib/beetle/import.rb
+++ lib/beetle/import.rb
@@ -4,24 +4,31 @@ module Beetle
     extend self
 
     def run
-      transformations.each do |t|
-        Transform.new(t.table_name, t.query).run
-      end
-
-      transformations.each do |t|
-        MapRelations.new(t.table_name, t.relations).run
-        TableDiff.new(t.table_name).run
-        AssignIds.new(t.table_name).run
-      end
+      data_steps.each(&:run)
 
       Beetle.database.transaction do
-        transformations.each do |t|
-          Load.new(t.table_name).run
-        end
+        load_steps.each(&:run)
       end
     end
 
     private
+
+    def data_steps
+      transformations.map do |t|
+        [
+          Transform.new(t.table_name, t.query),
+          MapRelations.new(t.table_name, t.relations),
+          TableDiff.new(t.table_name),
+          AssignIds.new(t.table_name),
+        ]
+      end.flatten
+    end
+
+    def load_steps
+      transformations.map do |t|
+        Load.new(t.table_name)
+      end
+    end
 
     def transformations
       @transformations ||= begin
--- spec/import_spec.rb
+++ spec/import_spec.rb
@@ -5,4 +5,3 @@ module Beetle
 
   end
 end
-
