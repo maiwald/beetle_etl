f20b0881f2c342132d241c9b444590c384fe1e04
Luciano Maiwald <luciano.maiwald@gmail.com>
2014-12-31 16:16:02 +0100

    use SQL in AssignIds step

--- lib/beetle_etl/steps/assign_ids.rb
+++ lib/beetle_etl/steps/assign_ids.rb
@@ -13,43 +13,33 @@ module BeetleETL
     end
 
     def assign_new_ids
-      stage_table
-        .where(
-          import_run_id: run_id,
-          transition: 'CREATE'
-        )
-        .update(
-          id: Sequel.function(:NEXTVAL, "public.#{table_name}_id_seq")
-        )
+      database.execute <<-SQL
+        UPDATE #{stage_table_name}
+        SET id = nextval('#{table_name}_id_seq')
+        WHERE import_run_id = #{run_id}
+        AND transition = 'CREATE'
+      SQL
     end
 
     def map_existing_ids
-      stage_table
-        .from(stage_table_identifier, public_table_identifier)
-        .where(
-          stage__import_run_id: run_id,
-          stage__transition: %w(KEEP UPDATE DELETE UNDELETE),
-          stage__external_id: :public__external_id
-        )
-        .update(id: :public__id)
+      database.execute <<-SQL
+        UPDATE #{stage_table_name} stage
+        SET id = public.id
+        FROM #{public_table_name} public
+        WHERE stage.import_run_id = #{run_id}
+        AND stage.transition IN ('KEEP', 'UPDATE', 'DELETE', 'UNDELETE')
+        AND stage.external_id = public.external_id
+      SQL
     end
 
     private
 
-    def stage_table_identifier
-      :"#{stage_schema}__#{table_name}___stage"
+    def stage_table_name
+      %Q("#{stage_schema}"."#{table_name}")
     end
 
-    def stage_table
-      database[stage_table_identifier]
-    end
-
-    def public_table_identifier
-      :"#{table_name}___public"
-    end
-
-    def public_table
-      database[public_table_identifier]
+    def public_table_name
+      %Q("#{table_name}")
     end
 
   end
