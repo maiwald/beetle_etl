c588946ada026a7a84b325b828019712be5289ea
Luciano Maiwald <luciano.maiwald@gmail.com>
2015-02-08 22:12:30 +0100

    rewrite and simplify feature test to cover all transitions

--- spec/feature/example_schema.rb
+++ spec/feature/example_schema.rb
@@ -18,26 +18,8 @@ module ExampleSchema
     test_database.create_table :source__Organisation do
       Integer :pkOrgId
       String :Name, size: 255
+      String :Adresse, size: 255
       String :Abteilung, size: 255
-    end
-
-    test_database.create_table :source__Person do
-      Integer :pkPersID
-      String :Vorname, size: 255
-      String :Nachname, size: 255
-      Integer :fkFirma
-      Integer :fkAdresse
-      Integer :fkTyp
-    end
-
-    test_database.create_table :source__Veranstaltung do
-      Integer :pkVeranstaltungId
-      Integer :fkOrganisation
-    end
-
-    test_database.create_table :source__Veranstaltungsbesuch do
-      Integer :fkVeranstaltung
-      Integer :fkBesucher
     end
   end
 
@@ -63,6 +45,7 @@ module ExampleSchema
       String :transition, size: 255
 
       String :name, size: 255
+      String :address, size: 255
     end
 
     test_database.create_table :stage__departments do
@@ -79,45 +62,6 @@ module ExampleSchema
 
     end
 
-    test_database.create_table :stage__attendees do
-      Integer :id
-      String :external_id, size: 255
-      foreign_key :import_run_id, :stage__import_runs
-      index [:external_id, :import_run_id]
-      String :transition, size: 255
-
-      String :first_name, size: 255
-      String :last_name, size: 255
-    end
-
-    test_database.create_table :stage__events do
-      Integer :id
-      String :external_id, size: 255
-      foreign_key :import_run_id, :stage__import_runs
-      index [:external_id, :import_run_id]
-      String :transition, size: 255
-
-      String :name, size: 255
-      DateTime :starts_at
-      DateTime :ends_at
-
-      String :external_organisations_id, size: 255
-      Integer :organisation_id
-    end
-
-    test_database.create_table :stage__attendees_events do
-      Integer :id
-      String :external_id, size: 255
-      foreign_key :import_run_id, :stage__import_runs
-      index [:external_id, :import_run_id]
-      String :transition, size: 255
-
-      String :external_attendee_id, size: 255
-      Integer :attendee_id
-
-      String :external_event_id, size: 255
-      Integer :event_id
-    end
   end
 
   def drop_stage_tables
@@ -130,6 +74,7 @@ module ExampleSchema
       String :external_id, size: 255
       String :external_source, size: 255
       String :name, size: 255
+      String :address, size: 255
       DateTime :created_at
       DateTime :updated_at
       DateTime :deleted_at
@@ -146,45 +91,9 @@ module ExampleSchema
       DateTime :deleted_at
     end
 
-    test_database.create_table :attendees do
-      primary_key :id
-      String :external_id, size: 255
-      String :external_source, size: 255
-      String :first_name, size: 255
-      String :last_name, size: 255
-      DateTime :created_at
-      DateTime :updated_at
-      DateTime :deleted_at
-    end
-
-    test_database.create_table :events do
-      primary_key :id
-      String :external_id, size: 255
-      String :external_source, size: 255
-      String :name, size: 255
-      DateTime :starts_at
-      DateTime :ends_at
-      foreign_key :organisation, :organisations
-      DateTime :created_at
-      DateTime :updated_at
-      DateTime :deleted_at
-    end
-
-    test_database.create_table :attendees_events do
-      foreign_key :attendee_id, :attendees, null: false
-      foreign_key :event_id, :events, null: false
-      primary_key [:attendee_id, :event_id]
-      index [:attendee_id, :event_id]
-      DateTime :created_at
-      DateTime :updated_at
-      DateTime :deleted_at
-    end
   end
 
   def drop_target_tables
-    test_database.drop_table :attendees_events
-    test_database.drop_table :events
-    test_database.drop_table :attendees
     test_database.drop_table :departments
     test_database.drop_table :organisations
   end
--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -1,4 +1,6 @@
 require 'spec_helper'
+require 'timecop'
+
 require_relative 'example_schema'
 require 'yaml'
 
@@ -9,18 +11,12 @@ describe BeetleETL do
 
   include ExampleSchema
 
-  let!(:now) { Time.new(2014, 07, 17, 16, 12).beginning_of_day }
-  before { allow(Time).to receive(:now) { now } }
+  let!(:time1) { Time.new(2014 , 7 , 17 , 16 , 12).beginning_of_day }
+  let!(:time2) { Time.new(2015 , 2 ,  8 , 22 , 18).beginning_of_day }
+  let!(:time3) { Time.new(2015 , 11 , 3 , 12 , 17).beginning_of_day }
 
-  before { create_tables }
-  after { drop_tables }
-
-  it 'is a working', :feature do
-    insert_into(:source__Organisation).values(
-      [ :pkOrgId , :Name   , :Abteilung ] ,
-      [ 1        , 'Apple' , 'iPhone'   ] ,
-      [ 2        , 'Apple' , 'MacBook'  ] ,
-    )
+  before :each do
+    create_tables
 
     database_config_path = File.expand_path('../support/database.yml', File.dirname(__FILE__))
     database_config = YAML.load(File.read(database_config_path))
@@ -31,21 +27,91 @@ describe BeetleETL do
       config.external_source = 'source_name'
       config.stage_schema = 'stage'
     end
+  end
 
+  after do
+    drop_tables
+  end
 
-    BeetleETL.import
+  it 'performs all possible transitions', :feature do
+    # create, keep, update, delete, undelete
 
+    import1
+    import2
+    import3
+  end
+
+  def import1
+    # create
+    insert_into(:source__Organisation).values(
+      [ :pkOrgId , :Name    , :Adresse        , :Abteilung ] ,
+      [ 1        , 'Apple'  , 'Apple Street'  , 'iPhone'   ] ,
+      [ 2        , 'Google' , 'Google Street' , 'MacBook'  ] ,
+      [ 3        , 'Audi'   , 'Audi Street'   , 'A4'       ] ,
+    )
+
+    Timecop.freeze(time1) do
+      BeetleETL.import
+    end
 
     expect(:organisations).to have_values(
-      [ :id , :external_id , :external_source , :name   , :created_at , :updated_at , :deleted_at ] ,
-      [ 1   , 'Apple'      , 'source_name'    , 'Apple' , now         , now         , nil         ]
+      [ :id                       , :external_id , :external_source , :name    , :address        , :created_at , :updated_at , :deleted_at ] ,
+      [ organisation_id('Apple')  , 'Apple'      , 'source_name'    , 'Apple'  , 'Apple Street'  , time1       , time1       , nil         ] ,
+      [ organisation_id('Google') , 'Google'     , 'source_name'    , 'Google' , 'Google Street' , time1       , time1       , nil         ] ,
+      [ organisation_id('Audi')   , 'Audi'       , 'source_name'    , 'Audi'   , 'Audi Street'   , time1       , time1       , nil         ]
     )
 
-    expect(:departments).to have_values(
-      [ :id , :external_id , :external_source , :name     , :organisation_id , :created_at , :updated_at , :deleted_at ] ,
-      [ 1   , '[Apple,1]'  , 'source_name'    , 'iPhone'  , 1                , now         , now         , nil         ] ,
-      [ 2   , '[Apple,2]'  , 'source_name'    , 'MacBook' , 1                , now         , now         , nil         ] ,
+    test_database[:source__Organisation].truncate
+  end
+
+  def import2
+    # keep, update, delete
+    insert_into(:source__Organisation).values(
+      [ :pkOrgId , :Name    , :Adresse            , :Abteilung ] ,
+      [ 1        , 'Apple'  , 'Apple Street'      , 'iPhone'   ] ,
+      [ 2        , 'Google' , 'NEW Google Street' , 'MacBook'  ] ,
+    # [ 3        , 'Audi'   , 'Audi Street'       , 'A4'       ] ,
     )
+
+    Timecop.freeze(time2) do
+      BeetleETL.import
+    end
+
+    expect(:organisations).to have_values(
+      [ :id                       , :external_id , :external_source , :name    , :address            , :created_at , :updated_at , :deleted_at ] ,
+      [ organisation_id('Apple')  , 'Apple'      , 'source_name'    , 'Apple'  , 'Apple Street'      , time1       , time1       , nil         ] ,
+      [ organisation_id('Google') , 'Google'     , 'source_name'    , 'Google' , 'NEW Google Street' , time1       , time2       , nil         ] ,
+      [ organisation_id('Audi')   , 'Audi'       , 'source_name'    , 'Audi'   , 'Audi Street'       , time1       , time2       , time2       ]
+    )
+
+    test_database[:source__Organisation].truncate
+  end
+
+  def import3
+    # undelete with update
+    insert_into(:source__Organisation).values(
+      [ :pkOrgId , :Name    , :Adresse            , :Abteilung ] ,
+      [ 1        , 'Apple'  , 'Apple Street'      , 'iPhone'   ] ,
+      [ 2        , 'Google' , 'NEW Google Street' , 'MacBook'  ] ,
+      [ 3        , 'Audi'   , 'NEW Audi Street'   , 'A4'       ] ,
+    )
+
+    Timecop.freeze(time3) do
+      BeetleETL.import
+    end
+
+    expect(:organisations).to have_values(
+      [ :id                       , :external_id , :external_source , :name    , :address            , :created_at , :updated_at , :deleted_at ] ,
+      [ organisation_id('Apple')  , 'Apple'      , 'source_name'    , 'Apple'  , 'Apple Street'      , time1       , time1       , nil         ] ,
+      [ organisation_id('Google') , 'Google'     , 'source_name'    , 'Google' , 'NEW Google Street' , time1       , time2       , nil         ] ,
+      [ organisation_id('Audi')   , 'Audi'       , 'source_name'    , 'Audi'   , 'NEW Audi Street'   , time1       , time3       , nil         ]
+    )
+
+    test_database[:source__Organisation].truncate
+  end
+
+  def organisation_id(external_id)
+    test_database[:organisations].first(external_id: external_id)[:id]
   end
 
 end
