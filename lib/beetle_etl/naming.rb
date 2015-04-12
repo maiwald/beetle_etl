3f66d4d74fcd4cb35e3d2bd1653cb10e7c011174
Luciano Maiwald <luciano.maiwald@gmail.com>
2015-04-12 14:52:12 +0200

    add source to stage tables names

--- lib/beetle_etl/naming.rb
+++ lib/beetle_etl/naming.rb
@@ -8,7 +8,7 @@ module BeetleETL
     def stage_table_name(table_name = nil)
       name = (table_name || @table_name).to_s
       digest = Digest::MD5.hexdigest(name)
-      "#{name}-#{digest}"[0, 63]
+      "#{BeetleETL.config.external_source}-#{name}-#{digest}"[0, 63]
     end
 
     def stage_table_name_sql(table_name = nil)
